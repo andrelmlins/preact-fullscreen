30c10f43da13d86d4dd88738065ff54d94c1a3f8
André Lins <andrelucas01@hotmail.com>
2020-05-24 19:29:35 -0300

    First Commit

---
+++ src/lib/FullScreen.js
@@ -0,0 +1,10 @@
+import { h } from "preact";
+
+const FullScreenComponent = () => (
+  <div class={style.home}>
+    <h1>Home</h1>
+    <p>This is the Home component.</p>
+  </div>
+);
+
+export default FullScreenComponent;
